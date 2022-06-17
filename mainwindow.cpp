73e4fd89f88b0e136599c25ef6b559261261d593
RPlsicik <rplsicik@gmail.com>
2022-06-17 11:58:05 +0200

    Init command 1v

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -237,7 +237,7 @@ void MainWindow::on_pushButton_4_clicked()
     qsizetype i = 0;
     if(txt.at(i) == QChar('x') || txt.at(i) == QChar('X')){
         qDebug()<<"Hex code has been detected" << txt;
-        bool ok;
+        //bool ok;
 
         QByteArray tst;
         QString dat;
@@ -271,7 +271,7 @@ void MainWindow::on_pushButton_4_clicked()
             }
             qDebug() << "Get mode command" << sendData;
         }
-        else if(QString::compare(txt,"readreg") == 0){
+        else if(QString::compare(txt,"readreg") == 0){  //Implement asking from fine adresse
            static const char initData[] = {
                 '\x0f', '\x12' , '\x00'
             };
@@ -282,10 +282,130 @@ void MainWindow::on_pushButton_4_clicked()
             }
             qDebug() << "Read reg command" << sendData;
         }
+
+        else if(QString::compare(txt,"init") == 0){
+            QByteArray sendData;
+
+            //For Set value is used command WriteReg-[0x12]  - EXCEPT FOR 1.8.9. step
+            //1-Set to Config Mode [0x02]
+            static const char Config[] = {
+                 '\x0f', '\x02' , '\x00'
+             };
+             sendData = QByteArray::fromRawData(Config,sizeof (Config));
+             while(!port1->waitForBytesWritten(300)){
+                 port1->write(Config );
+             }
+            //2-Set Reset Mode [0x00]on value 0x01   (by WriteReg[x12])
+             static const char ResetMod[] = {
+                  '\x0f', '\x00' , '\x01'
+              };
+              sendData = QByteArray::fromRawData(ResetMod,sizeof (ResetMod));
+              while(!port1->waitForBytesWritten(300)){
+                  port1->write(sendData);
+              }
+            //3-Set Clock divider [0x1F] on value 0xC0 (by WriteReg[x12])
+              static const char ClockDivData[] = {
+                   '\x0f', '\x1F' , '\xC0'
+               };
+               sendData = QByteArray::fromRawData(ClockDivData,sizeof (ClockDivData));
+               while(!port1->waitForBytesWritten(300)){
+                   port1->write(sendData);
+               }
+            //4-Set message filter; without filtration: (by WriteReg[x12])
+                //set-> Acceptance Code [?] on 0x00
+               static const char AccCode[] = {
+                    '\x0f', '\x12', '\x02' , '\x10' , '\x00'
+                };
+                sendData = QByteArray::fromRawData(AccCode,sizeof (AccCode));
+                while(!port1->waitForBytesWritten(300)){
+                    port1->write(sendData);
+                }
+
+                //set-> Acceptance Mask [0x05] on 0xff
+                static const char AccMask[] = {
+                     '\x0f', '\x12', '\x02' , '\x10' , '\xff'
+                 };
+                 sendData = QByteArray::fromRawData(AccMask,sizeof (AccMask));
+                 while(!port1->waitForBytesWritten(300)){
+                     port1->write(sendData);
+                 }
+
+            //5-Set OutputControl[0x08] on 0xDA (by WriteReg[x12])
+                 static const char OutCtrl[] = {
+                      '\x0f', '\x12', '\x02' , '\x10' , '\xda'
+                  };
+                  sendData = QByteArray::fromRawData(OutCtrl,sizeof (OutCtrl));
+                  while(!port1->waitForBytesWritten(300)){
+                      port1->write(sendData);
+                  }
+
+              //6-Set Bus Timing 0 [0x06] 1[0x07] to (1)?  (by WriteReg[x12])
+                  //Bus Timing 0 - to 1Mbit/s
+                  static const char BT0[] = {
+                       '\x0f', '\x20' , '\x02' , '\x06', '\x10'
+                   };
+                   sendData = QByteArray::fromRawData(BT0,sizeof (BT0));
+                   while(!port1->waitForBytesWritten(300)){
+                       port1->write(sendData);
+                   }
+                   //Bus Timing 1
+                   static const char BT1[] = {
+                        '\x0f', '\x20' , '\x02' , '\x07', '\x14'
+                    };
+                    sendData = QByteArray::fromRawData(BT1,sizeof (BT1));
+                    while(!port1->waitForBytesWritten(300)){
+                        port1->write(sendData);
+                    }
+              //7-Set Interrupt enable[]  on 0x03  (by WriteReg[x12])
+                    static const char IE[] = {
+                         '\x0f', '\x13', '\x02' , '\x04' , '\x03'
+                     };
+                     sendData = QByteArray::fromRawData(IE,sizeof (IE));
+                     while(!port1->waitForBytesWritten(300)){
+                         port1->write(sendData);
+                     }
+            //8-Set Transmit Critical Limit and Transmit Ready limit by cmd COMMAND
+                 //Set TCL
+                static const char TCL_Code[] = {
+                     '\x0f', '\x20' , '\x02' , '\x00', '\x12'
+                 };
+                 sendData = QByteArray::fromRawData(TCL_Code,sizeof (TCL_Code));
+                 while(!port1->waitForBytesWritten(300)){
+                     port1->write(sendData);
+                 }
+                 //set-> TCR
+                 static const char TCR_Code[] = {
+                      '\x0f', '\x20' , '\x02' , '\x00', '\x12'
+                  };
+                  sendData = QByteArray::fromRawData(TCR_Code,sizeof (TCR_Code));
+                  while(!port1->waitForBytesWritten(300)){
+                      port1->write(sendData);
+                  }
+            //9-Set Normal Mode
+                  static const char NormalMode[] = {
+                       '\x0f', '\x03' , '\x00'
+                   };
+                   sendData = QByteArray::fromRawData(NormalMode,sizeof (NormalMode));
+                   while(!port1->waitForBytesWritten(300)){
+                       port1->write(sendData);
+                   }
+            //10-Set Mode register [0x00], the value depends on Message Filter   (by WriteReg[x12])
+                //values can be 0x00 or 0x08
+                   static const char ModRegDat[] = {
+                        '\x0f', '\x12' , '\x02' , '\x00', '\x00'
+                    };
+                    sendData = QByteArray::fromRawData(ModRegDat,sizeof (ModRegDat));
+                    while(!port1->waitForBytesWritten(300)){
+                        port1->write(sendData);
+                    }
+
+            //After this steps is possible sending and receiving the CAN messages
+            qDebug() << "Init command" << sendData;
+        }
+
         else{
             qDebug()<< "Command does not exist";
         }
-
     }
 
     else{
@@ -414,7 +534,6 @@ void MainWindow::readSerial_2(){
 
 void MainWindow::readSerial_1(){
     //QString ReadData = port1->readAll();
-    bool ok;
     QByteArray ReadData = port1->readAll();
     //qDebug() << "Received_port1: " << QByteArray::fromHex(ReadData);
 
