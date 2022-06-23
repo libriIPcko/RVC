c8ad05c65d0de2aa7f6aaa9958c43d29369aa11a
RPlsicik <rplsicik@gmail.com>
2022-06-23 14:19:13 +0200

    usb2can_driver()
    C:\Users\RPlsicik\Documents\GitHub\RVC\usb2can_driver.h:40: error: Class declaration lacks Q_OBJECT macro.

--- usb2can_driver.cpp
+++ usb2can_driver.cpp
@@ -1,12 +1,71 @@
 #include "usb2can_driver.h"
 
 #include "QSerialPort"
+#include "QObject"
+#include "QDebug"
 
 
 
 USB2CAN_driver::USB2CAN_driver()
 {
+    port_USB2CAN = new QSerialPort();
+    QObject::connect(port_USB2CAN,SIGNAL(readyRead()),port_USB2CAN,SLOT(QByteArray read_USB2CAN()));
+}
 
 
+void USB2CAN_driver::USB_LoopBack(){
+
+}
+void USB2CAN_driver::Boot_Mode(){
+
+}
+void USB2CAN_driver::Config_Mode(){
+
+}
+void USB2CAN_driver::Normal_Mode(){
+
+}
+void USB2CAN_driver::LoopBack_Mode(){
+
+}
+QByteArray USB2CAN_driver::Get_Mode(){
+    while(port_USB2CAN->waitForBytesWritten(300)){
+        port_USB2CAN->write(getMode);
+
+    }
+
+    return port_USB2CAN->readAll();
+}
+
+void USB2CAN_driver::WriteReg(QByteArray regAdress, QByteArray value[]){
+    int length = regAdress.length() + value->length();
+    QByteArray len;
+    len.setNum(length);
+    QByteArray sendVal[] = { writeReg, len, regAdress, *value };
+    QByteArray sendData;
+    sendData.fromRawData(*sendVal,sizeof (sendVal));
+    while(port_USB2CAN->waitForBytesWritten(300)){
+        port_USB2CAN->write(sendData);
+
+    }
+}
+QByteArray USB2CAN_driver::WriteCMD(QByteArray CMD_name, QByteArray value){
+
+}
+QByteArray USB2CAN_driver::ReadReg(QByteArray regAdress){
+
+}
+
+int USB2CAN_driver::init(){
+    tim_interrupt_1 = new QTimer();
+    QObject::connect(tim_interrupt_1,SIGNAL(timeout()),tim_interrupt_1,SLOT(initSend()));
+}
+
+QByteArray USB2CAN_driver::read_USB2CAN(){
+    return port_USB2CAN->readAll();
+}
+
+void USB2CAN_driver::initSend(){
+    USB2CAN_driver::port_USB2CAN->write(Config);
 
 }
--- usb2can_driver.h
+++ usb2can_driver.h
@@ -2,21 +2,41 @@
 #define USB2CAN_DRIVER_H
 
 #include "QSerialPort"
+#include "QTimer"
+#include "USB2CAN_define.h"
+
+#include "QObject"
 
 
 class USB2CAN_driver
 {
+    //Q_OBJECT;
 public:
     USB2CAN_driver();
 
+    QSerialPort *port_USB2CAN = new QSerialPort();
+    QTimer *tim_interrupt_1 = new QTimer();
 
-    QSerialPort *port_USB2CAN;
+    int init();
+    void USB_LoopBack();
+    void Boot_Mode();
+    void Config_Mode();
+    void Normal_Mode();
+    void LoopBack_Mode();
+    QByteArray Get_Mode();
+
+    void WriteReg(QByteArray regAdress, QByteArray value[]);
+    QByteArray WriteCMD(QByteArray CMD_name, QByteArray value);
+    QByteArray ReadReg(QByteArray regAdress);
+
+
 
 private: signals:
     void readyRead();
 
 private slots:
-    void read_USB2CAN();
+    QByteArray read_USB2CAN();
+    void initSend();
 };
 
 #endif // USB2CAN_DRIVER_H
